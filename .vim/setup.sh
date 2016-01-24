3aa45a8c48d1ff2c775c7af1745fefe7401ca459
Mitesh Shah <miteshninja@gmail.com>
2016-01-24 21:29:42 +0530

    added setup files for tmux and vim folders

---
+++ .vim/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+DOTFILESDIR=~/Dropbox/Projects/dotfiles
+echo ".vim setup initialized."
+
+
+install_vim_plug () {
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+
+if [ -f ~/.vimrc ]; then
+	echo "Backing up existing vimrc."
+	mv ~/.vimrc ~/.vimrc.$(date +%F-%R).bak
+fi
+
+if [ -d ~/.vim ]; then
+	echo "Backing up existing .vim folder"
+	mv ~/.vim ~/.vim.$(date +%F-%R).bak
+fi
+
+
+echo "Linking .vim folder"
+ln --symbolic -v ${DOTFILESDIR}/.vim ~/
+
+
+while true; do
+    read -p "Do you wish to install vim-plug? [y/n]" yn
+    case $yn in
+        [Yy]* ) install_vim_plug; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer [y]es or [n]o.";;
+    esac
+done
---
+++ tmux/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+DOTFILESDIR=~/Dropbox/Projects/dotfiles
+if [ -f ~/.tmux.conf ]; then
+        echo "Creating .tmux.conf backup."
+        mv ~/.tmux.conf ~/.tmux.conf$(date +%F-%R).bak
+fi
+
+echo "Soft linking .tmux.conf to ${HOME} directory."
+ln --symbolic -v ${DOTFILESDIR}/tmux/.tmux.conf ${HOME}/
