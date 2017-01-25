bc26902efb7ddfd45d1337ca9f0aed5eb2c17879
Mitesh Shah <mitesh@miteshshah.com>
2017-01-26 02:54:57 +0530

    Saner script management

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+# @oxalorg
 
 dest=$HOME/.local/bin
 
-for f in */*
+# symlink all files in child folders to local bin
+for f in $(find . -mindepth 2 -type f -not -name README.md)
 do
-	src=`pwd`/$f
-	echo $src
-	cp $src $dest/`basename $f`	
-	chmod u+x $dest/`basename $f`
+    temp=`echo $f | cut --complement -c 1,2`
+    src=`pwd`/$temp
+    [ ! -e "$src" ] && ln -sv $src $dest/`basename $f` && chmod u+x $dest/`basename $f`
+done
+
+# clean bin directory
+for f in $dest/*; do
+    if [ ! -e "$f" ] ; then
+        # bin file/symlink is invalid
+        rm $f
+    fi
 done
