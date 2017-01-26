519fa78c26c6f209c33628857b8ae559fb327cd1
Mitesh Shah <mitesh@miteshshah.com>
2017-01-26 12:36:23 +0530

    Updated install script to ignore 'archived'

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 dest=$HOME/.local/bin
 
 # symlink all files in child folders to local bin
-for f in $(find . -mindepth 2 -type f -not -name README.md)
+for f in $(find . -mindepth 2 -type f -not -name README.md -not -path "*archived*")
 do
     temp=`echo $f | cut --complement -c 1,2`
     src=`pwd`/$temp
@@ -16,5 +16,6 @@ for f in $dest/*; do
     if [ ! -e "$f" ] ; then
         # bin file/symlink is invalid
         rm $f
+        echo "Removing broken link: $f"
     fi
 done
