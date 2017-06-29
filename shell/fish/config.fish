222a88e17fc2033092572854808fc1902b903431
Mitesh Shah <mitesh@oxal.org>
2017-06-29 23:28:35 +0530

    Trying out fish shell, bored/fedup of zsh

---
+++ shell/fish/config.fish
@@ -0,0 +1,6 @@
+set PATH $HOME/.local/bin $HOME/Library/Python/3.6/bin $HOME/.npm-global/bin $PATH
+
+set -gx LC_ALL en_US.UTF-8
+set -gx LC_CTYPE en_US.UTF-8
+
+set -gx DOTFILES $HOME/Dropbox/Projects/dotfiles
