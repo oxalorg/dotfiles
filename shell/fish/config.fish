3b12e76969353baa8777c3a7413ec1384c40db96
Mitesh Shah <mitesh@oxal.org>
2017-10-30 13:12:14 +0530

    Added useful fzf commands and paths. Removed fish.

--- shell/fish/config.fish
+++
@@ -1,6 +0,0 @@
-set PATH $HOME/.local/bin $HOME/Library/Python/3.6/bin $HOME/.npm-global/bin $PATH
-
-set -gx LC_ALL en_US.UTF-8
-set -gx LC_CTYPE en_US.UTF-8
-
-set -gx DOTFILES $HOME/Dropbox/Projects/dotfiles
