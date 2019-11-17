f474d1ef6a0f655d498c76d20749b8709ee393b9
Mitesh Shah <mitesh@miteshshah.com>
2019-11-17 16:32:13 +0530

    Added emacs config dir

---
+++ emacs.d/elpa/bind-key-20191110.416/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bind-key" "20191110.416" "A simple way to manage personal keybindings" 'nil :commit "df2528cd5116cdf2885f6f497511bf0946fe0825" :keywords '("keys" "keybinding" "config" "dotemacs") :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :url "https://github.com/jwiegley/use-package")
---
+++ emacs.d/elpa/evil-escape-20180910.1234/evil-escape-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-escape" "20180910.1234" "No description available." 'nil :commit "f4e9116bfbaac8c9d210c17ad488e0982291245f")
