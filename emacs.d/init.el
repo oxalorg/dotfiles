315b9a030a9f3fe3b29edbfe7eb91e2856cdf660
Mitesh Shah <mitesh@miteshshah.com>
2023-04-26 17:00:47 +0530

    Disable org center via visual fill

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -582,14 +582,6 @@
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
-(defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . efs/org-mode-visual-fill))
-
 (org-babel-do-load-languages
   'org-babel-load-languages
   '((emacs-lisp . t)
