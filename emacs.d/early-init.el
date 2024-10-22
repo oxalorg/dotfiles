902ec86f3e982416dfd31003db22cb0c20f6d59b
Mitesh Shah <mitesh@miteshshah.com>
2024-10-23 01:40:56 +0530

    clean up init.el

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,12 +1,53 @@
-;; Skip garbage collections during startup to speed things up. This is optional
-;; but nice to have.
+(message "Early init loading...")
 
 (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
       gc-cons-percentage 0.6)
 
+(setq package-enable-at-startup nil)
+
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold 33554432 ; 32mb
-                  gc-cons-percentage 0.1)))
+                  gc-cons-percentage 0.1)
+            (message "*** Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
 
-(setq package-enable-at-startup nil)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+
+(when (not (file-exists-p (expand-file-name "straight/versions/default.el" straight-base-dir)))
+  (straight-freeze-versions))
+
+;; (use-package corgi-packages
+;;   :straight (corgi-packages
+;;              :type git
+;;              :local "~/projects/corgi-packages"
+;;              ;; :host github
+;;              ;; :repo "corgi-emacs/corgi-packages"
+;;              ))
+
+;; (add-to-list #'straight-recipe-repositories 'corgi-packages)
+
+(message "Early init finished loading...")
+
+(provide 'early-init.el)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,138 @@
 (use-package json)
 (use-package request)
 
-(message "local.ox.el loading...")
+(message "ox's emacs initializing...")
+
+;; Allow Ctrl-u to scroll up a page like vim
+(setq evil-want-C-u-scroll t)
+
+(setq warning-minimum-level :error)
+(setq warning-minimum-log-level :error)
+
+(defun gaiwan-filter-local (files)
+  (seq-filter
+   (lambda (file)
+     (file-exists-p (expand-file-name file user-emacs-directory)))
+   files))
+
+;; Load corgi the old fashioned way
+(add-to-list 'load-path "~/projects/corgi-packages")
+
+(let ()
+  (use-package corgi-defaults)
+  (use-package corgi-editor)
+  (use-package corgi-commands)
+  (use-package corgi-clojure)
+  (use-package corgi-emacs-lisp)
+  (use-package corgi-stateline)
+  (use-package corgi-bindings)
+
+  (use-package corkey
+    :config
+    (corkey-mode 1)
+    (corkey/load-and-watch
+     (cons 'corgi-keys
+           (cons 'user-keys
+                 (gaiwan-filter-local
+                  (list
+                   (concat "user-keys." (user-login-name) ".el")
+                   (concat "user-keys." system-name ".el")
+                   (concat "user-keys." (user-login-name) "." system-name ".el")))))
+     (cons 'corgi-signals
+           (cons 'user-signals
+                 (gaiwan-filter-local
+                  (list
+                   (concat "user-signals." (user-login-name) ".el")
+                   (concat "user-signals." system-name ".el")
+                   (concat "user-signals." (user-login-name) "." system-name ".el"))))))))
+
+(use-package magit)
+(use-package org
+  :config
+  (require 'org-tempo))
+(use-package markdown-mode)
+(use-package yaml-mode)
+(use-package inf-clojure)
+(use-package hcl-mode)
+(use-package typescript-mode)
+(use-package dockerfile-mode)
+(use-package groovy-mode)
+(use-package buttercup)
+(use-package rainbow-mode)
+(use-package pkg-info)
+;;(use-package clj-refactor)
+
+(server-start)
+(global-display-line-numbers-mode 1)
+
+;; use with ,,<letter>, e.g. `,,g' runs (user/go)
+(set-register ?k "#_clj (do (require 'kaocha.repl) (kaocha.repl/run))")
+(set-register ?K "#_clj (do (require 'kaocha.repl) (kaocha.repl/run-all))")
+(set-register ?r "#_clj (do (require 'user :reload) (user/reset))")
+(set-register ?g "#_clj (user/go)")
+(set-register ?b "#_clj (user/browse)")
+(set-register ?c "#_clj ((requiring-resolve 'nextjournal.clerk/serve!) {})")
+(set-register ?, "#_clj (nextjournal.clerk/show! \"{{buffer-file-name}}\")")
+(set-register ?p "#_clj (user/portal)")
+(set-register ?P "#_cljs (user/portal)")
+
+(use-package color-theme-sanityinc-tomorrow
+  :config
+  (load-theme 'sanityinc-tomorrow-night t))
+
+(add-hook 'before-save-hook
+          (lambda ()
+            (when (derived-mode-p 'prog-mode)
+              (delete-trailing-whitespace))))
+
+;;(require 'corgi-clojure-cider-extras)
+;;(require 'corgi-cider-connection-indicator)
+
+(setq cider-connection-message-fn
+      nil )
+
+(setq recentf-max-saved-items 100)
+
+(when (executable-find "bb")
+  (corgi/cider-jack-in-babashka))
+(run-at-time nil (* 5 60) 'recentf-save-list)
+(corgi/enable-cider-connection-indicator)
+
+cider-connected-hook
+
+(use-package visual-fill-column)
+
+(with-current-buffer (get-buffer-create "*scratch-clj*")
+  (clojure-mode))
+
+(with-current-buffer (get-buffer-create "*scratch*")
+  (lisp-interaction-mode))
+
+(mapcar
+ (lambda (file)
+   (let ((local-config (expand-file-name file user-emacs-directory)))
+     (when (file-exists-p local-config)
+       (load-file local-config))))
+ (list "local.el"
+       (concat "local." (user-login-name) ".el")
+       (concat "local." system-name ".el")
+       (concat "local." (user-login-name) "." system-name ".el")))
+
+(load-file (expand-file-name "local.ox.el" user-emacs-directory))
+
+(use-package html-to-hiccup)
+
+(setq warning-minimum-level :warning)
+(setq warning-minimum-log-level :warning)
+
+(put-clojure-indent 'reflect/extend-signatures '(1 :form (1)))
+(put-clojure-indent 'sc.api/letsc '(1))
+
+;; (eval-after-load 'projectile
+;;   (setq projectile-project-root-files-bottom-up
+;;         (cons "deps.edn"
+;;               projectile-project-root-files-bottom-up)))
+
 ;; Allow Ctrl-u to scroll up a page like vim
 (setq evil-want-C-u-scroll t)
 
